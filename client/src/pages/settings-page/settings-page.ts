ef2fe0bfe65b011ca9ce703c1d49c9304ae09492
hiaux0 <30693990+hiaux0@users.noreply.github.com>
2020-04-10 23:56:57 +0200

    Fe/json tree (#8)
    * 📦 chore(): add jsoneditor
    
    * fix(router): display keys
    
    * feat(jsonTree): init and add to router
    
    * feat(settings) use json tree to edit and display

--- client/src/pages/settings-page/settings-page.ts
+++ client/src/pages/settings-page/settings-page.ts
@@ -1,42 +1,25 @@
+import { IJsonEditor } from 'common/components/json-tree/json-tree';
 import * as vocabConfig from '../../../../config.json';
 import { httpClient } from './../../common/http-client';
-
-interface ISourceSetting {
-  name: string
-  search_url: string
-}
 
 import './settings-page.scss'
 
 export class SettingsPage {
-  public message: string = 'Learn some Vocabs';
+  public vocabConfig: typeof vocabConfig;
 
-  private sourceSettings: ISourceSetting[] = [
-    { name: 'foo', search_url: 'https://hello-world.io' },
-    { name: 'bar', search_url: 'https://say-nice-things.io' },
-    { name: 'baz', search_url: 'https://vocab-app.io' },
-  ]
-
-  private selectedSourceSetting: ISourceSetting;
-
-  private vocabConfig: typeof vocabConfig;
+  public jsonEditor: IJsonEditor;
 
   private httpClient = httpClient;
 
-  async bind() {
+  async created() {
     const settingsResponse = (await this.httpClient.fetch('/resources/settings')) as unknown as typeof vocabConfig
     this.vocabConfig = settingsResponse;
-    this.vocabConfig.online_sources
-    this.vocabConfig.online_sources[0].search_url
-    this.vocabConfig.system_notification.duration_sec
-    this.vocabConfig.system_notification.interval_sec
-
   }
 
   private async saveSettings() {
     const saveResponse = (await this.httpClient.fetch('/resources/settings', {
       method: 'POST',
-      body: JSON.stringify(this.vocabConfig),
+      body: JSON.stringify(this.jsonEditor.get()),
     })) as unknown as typeof vocabConfig;
   }
 }
