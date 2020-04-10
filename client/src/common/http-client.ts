d0fb3d1584c39dcb5b6e9bcd2cf9c1d2c5dfc938
hdn <hieudn5293@yahoo.de>
2020-04-10 16:35:14 +0200

    feat(httpClient): init

---
+++ client/src/common/http-client.ts
@@ -0,0 +1,32 @@
+import { HttpClient, json } from 'aurelia-fetch-client';
+import * as appConfigDefault from '../../app-config.json';
+
+const appConfig = JSON.parse(JSON.stringify(appConfigDefault)).default as unknown as typeof appConfigDefault
+const { Authorization, baseUrl } = appConfig;
+
+
+export let httpClient = new HttpClient();
+
+httpClient.configure(config => {
+  config
+    .withBaseUrl(baseUrl || 'api/v1')
+    .withDefaults({
+      credentials: 'same-origin',
+      headers: {
+        'Accept': 'application/json',
+        Authorization,
+        'Content-Type': 'application/json',
+        'X-Requested-With': 'Fetch',
+      }
+    })
+    .withInterceptor({
+      request(request) {
+        console.log(`Requesting ${request.method} ${request.url}`);
+        return request;
+      },
+      response(response) {
+        console.log(`Received ${response.status} ${response.url}`);
+        return response.json();
+      }
+    });
+});
