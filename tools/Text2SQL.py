a50c2427d615d3a1c34756dd978b1efa159edb9e
qh.ngo <hnhhoan@gmail.com>
2020-04-11 13:07:58 +0200

    I have made some changes on DB structure in order to improve query speed and added some functions for testing REST API

--- tools/Text2SQL.py
+++ tools/Text2SQL.py
@@ -2,20 +2,13 @@
 from DB_Dictionary import DB_SqliteDictionary
 from DB_Dictionary import TB_LanguageRecord
 from DB_Dictionary import TB_LanguageCollection
-from DB_Dictionary import TB_SampleSentenceRecord
-from DB_Dictionary import TB_SampleSentenceCollection
-from DB_Dictionary import TB_TagRecord
-from DB_Dictionary import TB_TagCollection
-from DB_Dictionary import TB_TypeRecord
-from DB_Dictionary import TB_TypeCollection
 from DB_Dictionary import TB_VocabularyRecord
 from DB_Dictionary import TB_VocabularyCollection
 from DB_Dictionary import TB_VocabularyDenotationRecord
 from DB_Dictionary import TB_VocabularyDenotationCollection
-from DB_Dictionary import TB_VocabularyDenotationTagRecord
-from DB_Dictionary import TB_VocabularyDenotationTagCollection
+from DB_Dictionary import QueryExecuter
 
-db_Dict = DB_SqliteDictionary("DB_Dictionary.db")
+db_Dict = DB_SqliteDictionary("DB_Dictionary_V1.db")
 conn = db_Dict.createConnection()
 
 
@@ -33,10 +26,7 @@ def lineTextAnalyzis(line):
         vocabulary = values[0]
         meaning = values[1]
         type = values[2]
-        str_tags = values[3]
-        tags = str_tags.split(" ")
-        if len(values) == 1:
-            tags = None
+        tags = values[3]
         return vocabulary, meaning, type, tags  # (str, str, str, list of str/None)
     else:
         return None
@@ -44,15 +34,12 @@ def lineTextAnalyzis(line):
 
 def loadDataInDatabase(file_path, languageA, languageB):
     with open(file_path, 'r', encoding="utf-8", errors="surrogateescape") as fp:
-        poolSize = 30
+        poolSize = 2
         vocabularyPool = []
         index = 0
         tb_LanguageColl = TB_LanguageCollection(conn)
         tb_VocabularyColl = TB_VocabularyCollection(conn)
         tb_VocabularyDenotationColl = TB_VocabularyDenotationCollection(conn)
-        tb_VocabularyDenotationTagColl = TB_VocabularyDenotationTagCollection(conn)
-        tb_TypeColl = TB_TypeCollection(conn)
-        tb_TagColl = TB_TagCollection(conn)
         records = tb_LanguageColl.getDataByCondition(" Language= '{0}'".format(languageA))
         if len(records) >= 1:
             idLanguageA = records[0].id
@@ -66,18 +53,18 @@ def loadDataInDatabase(file_path, languageA, languageB):
         else:
             n_Record = TB_LanguageRecord(None, languageB)
             idLanguageB = tb_LanguageColl.insertRecord(n_Record)
-        lineIndex=0
-        skipIndex=0
+        lineIndex = 0
+        skipIndex = 0
         for line in fp:
-            lineIndex=lineIndex+1
+            lineIndex = lineIndex + 1
             print(lineIndex)
             # skip the comment lines, which starts with #
             if line.startswith("#") or len(line) == 0:
-                skipIndex=skipIndex+1
+                skipIndex = skipIndex + 1
                 continue
             tupelValues = lineTextAnalyzis(line)
             if tupelValues is None:
-                skipIndex=skipIndex+1
+                skipIndex = skipIndex + 1
                 continue
             vocabulary = tupelValues[0]
             # get primary key of vocabulary in the database
@@ -107,41 +94,16 @@ def loadDataInDatabase(file_path, languageA, languageB):
                     n_Record.id = idVocabulary
                     vocabularyPool.pop(0)
                     vocabularyPool.append(n_Record)
-
-            # get primary key of type
-            type = tupelValues[2]
-            if len(type.strip()) == 0:
-                idType = None
-            else:
-                records = tb_TypeColl.getDataByCondition(""" Type= "{0}" """.format(escapeSpecialCharracter(type)))
-                if len(records) >= 1:
-                    idType = records[0].id
-                else:
-                    n_Record = TB_TypeRecord(None, type)
-                    idType = tb_TypeColl.insertRecord(n_Record)
-
             # insert meaning into the database with id key of the vocabulary'''
             meaning = tupelValues[1]
-            n_Record = TB_VocabularyDenotationRecord(None, idVocabulary, idType, idLanguageB, meaning)
-            idVocabularyDenotation = tb_VocabularyDenotationColl.insertRecord(n_Record)
+            type = tupelValues[2]
             tags = tupelValues[3]
-            n_Records = []
-            for tag in tags:
-                if len(tag.strip()) > 0:
-                    records = tb_TagColl.getDataByCondition(
-                        """ Tag="{0}" AND ID_Language={1}""".format(tag, idLanguageB))
-                    if len(records) >= 1:
-                        idTag = records[0].id
-                    else:
-                        n_Record = TB_TagRecord(None, tag, idLanguageB)
-                        idTag = tb_TagColl.insertRecord(n_Record)
-                    n_Record = TB_VocabularyDenotationTagRecord(None, idVocabularyDenotation, idTag)
-                    n_Records.append(n_Record)
-            tb_VocabularyDenotationTagColl.insertCollection(n_Records)
+            n_Record = TB_VocabularyDenotationRecord(None, idVocabulary, type, idLanguageB, meaning, tags)
+            idVocabularyDenotation = tb_VocabularyDenotationColl.insertRecord(n_Record)
         conn.commit()
-        print("skipped Lines from {0} :{1}/{2}".format(file_path,skipIndex,lineIndex))
+        conn.close()
+        print("skipped Lines from {0} :{1}/{2}".format(file_path, skipIndex, lineIndex))
 
 
-loadDataInDatabase("dicts\d-e.txt", "deutsch", "english")
-loadDataInDatabase("dicts\e-d.txt", "english", "deutsch")
+# loadDataInDatabase("..\dicts\d-e.txt", "deutsch", "english")
 print("Bitte überprüfen")
