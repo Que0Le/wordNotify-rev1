834183c64835b1ef6e717caaa2a97e81e5fa6419
hdn <hieudn5293@yahoo.de>
2020-04-18 18:07:31 +0200

    feat(dicts): add view and connect first enpoint

--- client/src/pages/dicts/dicts.ts
+++ client/src/pages/dicts/dicts.ts
@@ -1,14 +1,18 @@
+import { IApiResponse } from './../../../@types/api';
+import { IDict } from './../../../@types/index.d';
+import { endpoints } from './../../common/endpoints';
 import { httpClient } from '../../common/http-client';
 
 export class Dicts {
   message: string;
+  dicts: IDict[];
 
   constructor() {
     this.message = 'Dicts';
   }
 
   async created() {
-    // const response = await httpClient.fetch('/resources/dicts?dict_db=DE_FR&random=true')
-    // console.log("TCL: Dicts -> created -> response", response)
+    const response = await httpClient.fetch(endpoints.dicts.range(1, 5)) as unknown as IApiResponse<IDict[]>
+    this.dicts = response.response
   }
 }
