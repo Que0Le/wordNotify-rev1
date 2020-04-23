895a69b4a3cc526cb7da9bc5d871f1f82b9b515e
tuilagio <lebanhat95@gmail.com>
2020-04-23 21:35:48 +0200

    cleanup notifier

--- flaskr/localClass.py
+++ flaskr/localClass.py
@@ -54,6 +54,33 @@ class NotifierThead(threading.Thread):
         except:
             print("error opening web")
 
+    def get_random_word(self, global_config):
+        rand_dict_index = random.randint(
+            0, len(global_config["notification"]["dict_dbs_to_notify"])-1)
+        rand_dict_name = global_config["notification"]["dict_dbs_to_notify"][rand_dict_index]
+        rand_dict_id = -1
+        # Get all dict to find out id
+        json_response, url_full = handyfunctions.get_dict_by_param(
+            encoded_u=current_app.config["ENCODED_U"],
+            dict_id="")
+        if not json_response:
+            print("could not fetch from server!")
+            time.sleep(1)
+            return None, None, None, None
+        for r in json_response["response"]:
+            if r["table_name"] == rand_dict_name:
+                rand_dict_id = r["w_id"]
+                break
+        if rand_dict_id<0:
+            print(f"dict with name {rand_dict_name} not found in server!")
+            time.sleep(1)
+            return None, None, None, None
+        json_response, url_full = handyfunctions.get_word_by_param(
+            encoded_u=current_app.config["ENCODED_U"], 
+            dict_id=rand_dict_id,
+            word_id="random")
+        return rand_dict_id, rand_dict_name, json_response, url_full
+
     def run(self):
         with self.app.app_context():
             last_show = datetime.now()
@@ -71,30 +98,12 @@ class NotifierThead(threading.Thread):
                     continue
                 # Get data to push:
                 try:
-                    rand_dict_index = random.randint(
-                        0, len(global_config["notification"]["dict_dbs_to_notify"])-1)
-                    rand_dict_name = global_config["notification"]["dict_dbs_to_notify"][rand_dict_index]
-                    rand_dict_id = -1
-                    # Get all dict to find out id
-                    json_response, url_full = handyfunctions.get_dict_by_param(
-                        encoded_u=current_app.config["ENCODED_U"],
-                        dict_id="")
-                    if not json_response:
-                        print("could not fetch from server!")
+                    rand_dict_id, rand_dict_name, json_response, url_full = \
+                        self.get_random_word(global_config)
+                    if not (rand_dict_id and rand_dict_name and json_response and url_full):
+                        print("Could not get_random_word")
                         time.sleep(1)
                         continue
-                    for r in json_response["response"]:
-                        if r["table_name"] == rand_dict_name:
-                            rand_dict_id = r["w_id"]
-                            break
-                    if rand_dict_id<0:
-                        print(f"dict with name {rand_dict_name} not found in server!")
-                        time.sleep(1)
-                        continue
-                    json_response, url_full = handyfunctions.get_word_by_param(
-                        encoded_u=current_app.config["ENCODED_U"], 
-                        dict_id=rand_dict_id,
-                        word_id="random")
                 except:
                     traceback.print_exc()
                     print('error: getting notification material.')
@@ -104,11 +113,13 @@ class NotifierThead(threading.Thread):
                 notify_w_id = json_response["response"][0]["w_id"]
                 title_name = global_config["notification"]["entries_to_notify"][0]
                 notify_title = json_response["response"][0][title_name] + f" (from {rand_dict_name})"
+                # Construct content
                 notify_content = ""
                 for entry in global_config["notification"]["entries_to_notify"][1:]:
                     if json_response["response"][0][entry]:
                         notify_content += json_response["response"][0][entry] + "\n"
-                
+                # Construct link onclick
+                u = handyfunctions.url_base + "/api/v1/dicts/" + str(rand_dict_id) + "/words/" + str(notify_w_id)
                 if platform == "linux":
                     from plyer import notification
                     try:
@@ -147,7 +158,6 @@ class NotifierThead(threading.Thread):
                     toast = ToastNotifier()
                     try:
                         # http://127.0.0.1:5000/dicts/6/words/13359
-                        u = handyfunctions.url_base + "/dicts/" + str(rand_dict_id) + "/words/" + str(notify_w_id)
                         toast.show_toast(
                             title=notify_title,
                             msg=notify_content,
